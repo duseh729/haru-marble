13214ae30ba7ebc248ec7ea7a4e781771ae554f8
안도연 <duseh729@naver.com>
2026-03-04 00:38:56 +0900

    feat: kakao social 로그인 연동

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import LandingPage from './pages/LandingPage';
 import AppPage from './pages/AppPage';
 import CollectionPage from './pages/CollectionPage';
 import SettingsPage from './pages/SettingsPage';
+import AuthCallbackPage from './pages/AuthCallbackPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import MobileLayout from './components/MobileLayout';
@@ -24,6 +25,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="/auth/callback" element={<AuthCallbackPage />} />
           <Route
             path="/app"
             element={
